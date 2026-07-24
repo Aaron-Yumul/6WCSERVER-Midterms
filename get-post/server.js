88e1f62b76b198269c2d484ed42f69431a508e67
Aaron John Yumul <aaronjohnyumul526@gmail.com>
2026-07-24 16:35:39 +0800

    Add files via upload

---
+++ get-post/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/process-post', (req, res) => {
+  const { first_name, last_name } = req.body;
+
+  if (!first_name || !last_name) {
+    return res.status(400).json({
+      error: 'Both first_name and last_name are required.'
+    });
+  }
+
+  res.json({
+    message: 'Form submitted successfully.',
+    first_name,
+    last_name
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, world!');
+// });
+
+// app.get('/user', (req, res) => {
+//   const userId = req.query.id;
+
+//   if (!userId) {
+//     return res.status(400).send('The id query parameter is required.');
+//   }
+
+//   res.send(`User ID is ${userId}`);
+// });
+
